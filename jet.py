85c696fa00e9529c8dd7b6a1d418d8104469ecdc
Connor Swain <connor@gata.(none)>
2014-08-14 14:35:04 +0100

    Commit is working with list showing commit diffs

--- jet.py
+++ jet.py
@@ -8,46 +8,153 @@ from os import walk
 
 
 def push():
+    if not already_initialized():
+        print "Please init a jet repo before calling other commands"
+        return
     print "Pushed"
 
 def pull():
+    if not already_initialized():
+       print "Please init a jet repo before calling other commands"
+       return
     print "Pulled"
 
+
+def list_commits():
+    if not already_initialized():
+        print "Please init a jet repo before calling other commands"
+        return
+
+    valud=True
+    try:
+        commit_number = sys.argv[2]
+    except IndexError:
+        commits = get_immediate_subdirectories(os.path.join(os.getcwd() + '/.jet/'))
+        print "List of all commits. Type jet list <commit_number> to see more information"
+        commits.reverse()
+        for commit in commits:
+            print commit
+        return
+    commits = get_immediate_subdirectories(os.path.join(os.getcwd() + '/.jet/'))
+    found=False
+    for commit in commits:
+        if commit == commit_number:
+            found=True
+            with open ('.jet/%s/file_log.txt' % commit_number, 'r') as file_:
+                lines = file_.read().splitlines()
+            print "Changelog for commit number %s" % commit_number
+            for line in lines:
+                print line
+            
+    
+    if not found:
+        print "Incorrect commit number, type '$jet list' to see all commits"
+    
 def commit():
-    print sys.argv[2]
-    print sys.argv[3]
+    if not already_initialized():
+        print "Please init a jet repo before calling other commands"
+        return
+    #print sys.argv[2]
+    #print sys.argv[3]
     if len(sys.argv) != 4 or sys.argv[2]!= "-m":
         print "Commit commands need to be formed by typing: $jet commit -m \"Your message here\""
     else:
-        print "Commited"
+        if os.path.isfile(os.path.join(os.getcwd() + '/.jet/changeset.txt')):
+            new_files_in_changeset = get_new_files_in_changeset()
+            deleted_files_in_changeset = get_deleted_files_in_changeset()
+            new_commit_number = get_new_commit_number()
+            os.mkdir('.jet/%s/' % new_commit_number)
+            with open ('.jet/%s/file_log.txt' % new_commit_number, 'w') as file_:
+                for file_to_add in new_files_in_changeset:
+                    file_.write("+" + file_to_add + "\n")
+                for file_to_add in deleted_files_in_changeset:
+                    file_.write("-" + file_to_add + "\n")
+            os.remove(os.path.join(os.getcwd() + '/.jet/changeset.txt'))
+            with open('.jet/latest_saved_files.txt', 'r') as file_:
+                lines = file_.read().splitlines()
+            lines.extend(new_files_in_changeset)
+            to_keep = []
+            for line in lines:
+                if line not in deleted_files_in_changeset:
+                    to_keep.append(line)
+            os.remove(os.path.join(os.getcwd() + '/.jet/latest_saved_files.txt'))
+            with open('.jet/latest_saved_files.txt', 'w') as file_:
+                for file_to_add in to_keep:
+                    file_.write(file_to_add + "\n")
+            print "Commiting"
+        else:
+            print "Please add files to commit using jet add before commiting!"
 
 def add():
+    if not already_initialized():
+        print "Please init a jet repo before calling other commands"
+        return
     # At the moment, adds all of the files, however will eventually be able to choose!
     print "Added to changeset"
+    with open ('.jet/changeset.txt', 'w') as file_:
+        for file_to_add in get_new_files():
+            file_.write("+" + file_to_add + "\n")
+        for file_to_add in get_deleted_files():
+            file_.write("-" + file_to_add + "\n")
 
 def merge():
+    if not already_initialized():
+        print "Please init a jet repo before calling other commands"
+        return
     print "Merged"
 
 def status():
+    if not already_initialized():
+        print "Please init a jet repo before calling other commands"
+        return
     if len(get_current_files()) == len(get_stored_files()):
         print "Nothing has changed!"
     else:
         print "Your repo has changed since the last commit"
-        print "You have added:"
-        for new_file in get_new_files():
-            print "    %s" % new_file
-        print "You have deleted:"
-        for deleted_file in get_deleted_files():
-            print "    %s" % deleted_file
+        new_files = get_new_files()
+        if new_files:
+            new_files_in_changeset = get_new_files_in_changeset()
+            if new_files_in_changeset:
+                unadded_new = []
+                for new_file in new_files:
+                    if new_file not in new_files_in_changeset:
+                        unadded_new.append(new_file)
+                print "New files in changeset:"
+                for new_file_in_changeset in new_files_in_changeset:
+                    print "    %s" % new_file_in_changeset
+            else:
+                unadded_new = new_files
+            if unadded_new:
+                print "New files:"
+                for new_file in unadded_new:
+                    print "    %s" % new_file
+        deleted_files = get_deleted_files()
+        if deleted_files:
+            deleted_files_in_changeset = get_deleted_files_in_changeset()
+            if deleted_files_in_changeset:
+                unadded_deleted = []
+                for deleted_file in deleted_files:
+                    if deleted_file not in deleted_files_in_changeset:
+                        unadded_deleted.append(deleted_file)
+                print "Deleted files in changeset:"
+                for deleted_file_in_changeset in deleted_files_in_changeset:
+                    print "    %s" % deleted_file_in_changeset
+            else:
+                unadded_deleted = deleted_files
+            if unadded_deleted:
+                print "Deleted files:"
+                for deleted_file in unadded_deleted:
+                    print "    %s" % deleted_file
         
 
+def help_text():
+    if not already_initialized():
+        print "Please init a jet repo before calling other commands"
+        return
+    print "For help, please visit www.jet.com/help"
+
 def init():
-    directory = os.getcwd()
-    already_done = False
-    for filename in os.listdir(directory):
-        if filename == ".jet":
-            already_done = True
-    if already_done:
+    if already_initialized():
         print "Already a repo initialized"
     else:
         os.mkdir('.jet')
@@ -55,11 +162,19 @@ def init():
         for (dirpath, dirnames, filenames) in walk(os.getcwd()):
             for filename in filenames:
                 f.append(os.path.join(dirpath, filename))
-        with open('.jet/current_files.txt', 'w') as file_:
+        with open('.jet/latest_saved_files.txt', 'w') as file_:
+            for file_to_add in f:
+                file_.write(file_to_add + "\n")
+        os.mkdir('.jet/0/')
+        with open('.jet/0/file_list.txt', 'w') as file_:
             for file_to_add in f:
                 file_.write(file_to_add + "\n")
 
-        print "Initializing Jet repository in %s" % directory
+        print "Initializing Jet repository in %s" % os.getcwd()
+
+def get_immediate_subdirectories(directory):
+    return [name for name in os.listdir(directory)
+            if os.path.isdir(os.path.join(directory, name))]
 
 def get_current_files():
     file_list = []
@@ -85,14 +200,17 @@ def get_current_files():
     return current_files
 
 
+def get_new_commit_number():
+    commits = get_immediate_subdirectories(os.path.join(os.getcwd() + '/.jet/'))
+    latest = commits[-1]
+    int_latest = int(latest)
+    new_commit_number = int_latest + 1
+    return new_commit_number
+
 def get_stored_files():
-    with open('.jet/current_files.txt', 'r') as myFile:
+    with open('.jet/latest_saved_files.txt', 'r') as myFile:
         lines = myFile.read().splitlines()
-        #print lines
-        stored_files = []
-        for line in lines:
-            stored_files.append(line)
-    return stored_files
+    return lines
 
 def get_new_files():
     current_files = get_current_files()
@@ -103,6 +221,32 @@ def get_new_files():
             new_files.append(current_file)
     return new_files
 
+def get_new_files_in_changeset():
+    try:
+        with open('.jet/changeset.txt', 'r') as myFile:
+            lines =myFile.read().splitlines()
+    except IOError:
+        return []
+    new_files = []
+    for line in lines:
+        if line.startswith('+'):
+            new_files.append(line[1:])
+    return new_files
+
+
+
+def get_deleted_files_in_changeset():
+    try:
+        with open('.jet/changeset.txt', 'r') as myFile:
+            lines = myFile.read().splitlines()
+    except IOError:
+        return []
+    deleted_files = []
+    for line in lines:
+        if line.startswith('-'):
+            deleted_files.append(line[1:])
+    return deleted_files
+
 def get_deleted_files():
     current_files = get_current_files()
     stored_files = get_stored_files()
@@ -111,6 +255,14 @@ def get_deleted_files():
         if stored_file not in current_files:
             deleted_files.append(stored_file)
     return deleted_files
+
+def already_initialized():
+    directory = os.getcwd()
+    already_done = False
+    for filename in os.listdir(directory):
+        if filename == ".jet":
+            already_done = True
+    return already_done
 
 
 
@@ -124,12 +276,15 @@ def get_deleted_files():
 
 
 commands = {
+    "add": add,
     "push": push,
     "pull": pull,
     "commit": commit,
     "merge": merge,
     "status": status,
     "init": init,
+    "help": help_text,
+    "list": list_commits,
 }
 
 try:
