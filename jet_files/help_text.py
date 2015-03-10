36918716b0fefffe595d082e699c061e7b61b709
Connor Swain <conninie@gmail.com>
2015-03-10 12:03:48 +0000

    Added comments

--- jet_files/help_text.py
+++ jet_files/help_text.py
@@ -5,6 +5,7 @@ def help_text():
     if not hf.already_initialized():
         print "Please init a jet repo before calling other commands"
         return
+    # No point re-writing documentation, so just link to it.
     print "For help, please visit www.jetvc.co.uk/documentation/"
 
 
