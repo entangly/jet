bb37e4bf7349245e9133aff438c2ec2eedba3147
Connor Swain <conninie@gmail.com>
2015-04-09 10:17:20 +0100

    Added comments

--- jet_files/local_tests.py
+++ jet_files/local_tests.py
@@ -1,4 +1,6 @@
-#Purpose of this file is to test the local version of the version control
+# Purpose of this file is to test the local version of the version control
+# RESULTS array stores either "PASSED" or the error message, depending on the result of the test
+# At the end, the python file prints the RESULT array, and gives a count of the error messages
 import os
 import shutil
 from jet_files import helper_functions, init, add, commit_changeset
